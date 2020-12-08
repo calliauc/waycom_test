b251ae55c158abe6be27e0ac3915018fd6034402
calliauc <contact@calliau.io>
2020-12-09 00:40:57 +0100

    file read

---
+++ pic_appel/pic_appel.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+import sys
+
+def generate_calls():
+    """return a list of calls based on the standard input"""
+    calls = []
+    for line in sys.stdin:
+        call = []
+        call += line.split(sep=':')
+        call[0] = int(call[0])
+        call[1] = int(call[1][:-1])
+        calls.append(call)
+    return calls
+
+
+
+def main():
+    calls = generate_calls()
+    print(f"{calls}")
+
+
+main()
